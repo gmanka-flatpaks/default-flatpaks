5d0da7339cec00ffcb58c525e6c34f944177ce5c
gmanka <gmankab@gmail.com>
2025-07-08 10:38:20 +0500

    [+] crash script if dis does not exist

--- default-flatpaks.sh
+++ default-flatpaks.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+if [[ ! -d /var/lib/default-flatpaks ]]; then
+    exit 1
+fi
 notify-send 'Installing default applications' 'You can check status with `systemctl status default-flatpaks`'
 xargs -a /etc/default-flatpaks/apps-list -- flatpak install --system -y flathub
 notify-send 'Default applications installed'
