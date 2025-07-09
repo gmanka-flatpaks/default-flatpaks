1d630e993f503c6b1618525549ee2a1266a3b48c
gmanka <gmankab@gmail.com>
2025-07-09 18:48:30 +0500

    [u] install script

--- default-flatpaks.sh
+++ default-flatpaks.sh
@@ -6,5 +6,4 @@ ls /var/lib/default-flatpaks
 notify-send 'Installing default applications' 'You can check status with `systemctl status default-flatpaks`'
 xargs -a /etc/default-flatpaks/apps-list -- flatpak install --system -y flathub
 notify-send 'Default applications installed'
-touch /var/lib/default-flatpaks/done
-
+touch /var/lib/default-flatpaks/initialized
