0e52789390213519614deacbdd616b129f1f973b
gmanka <gmankab@gmail.com>
2025-08-09 10:01:46 +0500

    [r] refactor, [u] spec 1.8

--- default-flatpaks.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -uexo pipefail
-
-ls /var/lib/default-flatpaks
-notify-send 'Installing default applications' 'You can check status with `systemctl status default-flatpaks`'
-xargs -a /etc/default-flatpaks/apps-list -- flatpak install --system -y flathub
-notify-send 'Default applications installed'
-touch /var/lib/default-flatpaks/initialized
